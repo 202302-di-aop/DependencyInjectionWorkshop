091940e9c3a2559a14d43917ed58f70e032fbf00
Joey Chen <jay.hatelove@gmail.com>
2022-11-19 17:22:11 +0800

    clean up

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -11,34 +11,6 @@ namespace DependencyInjectionWorkshop.Models
         bool IsValid(string account, string password, string otp);
     }
 
-    public class NotificationDecorator : IAuthentication
-    {
-        private readonly IAuthentication _authentication;
-        private readonly INotification _notification;
-
-        public NotificationDecorator(INotification notification, IAuthentication authentication)
-        {
-            _notification = notification;
-            _authentication = authentication;
-        }
-
-        public bool IsValid(string account, string password, string otp)
-        {
-            var isValid = _authentication.IsValid(account, password, otp);
-            if (!isValid)
-            {
-                Notify(account);
-            }
-
-            return isValid;
-        }
-
-        private void Notify(string account)
-        {
-            _notification.Notify(account, $"account:{account} try to login failed");
-        }
-    }
-
     public class AuthenticationService : IAuthentication
     {
         private readonly IFailedCounter _failedCounter;
