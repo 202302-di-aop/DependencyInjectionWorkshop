e7371c8157596e227e04792e933d8284565dbb14
Joey Chen <jay.hatelove@gmail.com>
2022-11-19 10:48:54 +0800

    feat: add failed counter log when invalid

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -8,6 +8,7 @@ using System.Net.Http;
 using System.Security.Cryptography;
 using System.Text;
 using Dapper;
+using NLog;
 using SlackAPI;
 
 #endregion
@@ -18,16 +19,15 @@ namespace DependencyInjectionWorkshop.Models
     {
         public bool IsValid(string account, string password, string otp)
         {
-            
             var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
             var isLockedResponse = httpClient.PostAsJsonAsync("api/failedCounter/IsLocked", account).Result;
 
             isLockedResponse.EnsureSuccessStatusCode();
             if (isLockedResponse.Content.ReadAsAsync<bool>().Result)
             {
-                throw new FailedTooManyTimesException(){Account = account};
+                throw new FailedTooManyTimesException() { Account = account };
             }
-            
+
             string passwordFromDb;
             using (var connection = new SqlConnection("my connection string"))
             {
@@ -54,16 +54,25 @@ namespace DependencyInjectionWorkshop.Models
 
             var currentOtp = response.Content.ReadAsAsync<string>().Result;
             if (passwordFromDb == hashedPassword && otp == currentOtp)
-            { 
-                var resetResponse = httpClient.PostAsJsonAsync("api/failedCounter/Reset", account).Result; 
+            {
+                var resetResponse = httpClient.PostAsJsonAsync("api/failedCounter/Reset", account).Result;
                 resetResponse.EnsureSuccessStatusCode();
-                
+
                 return true;
             }
             else
             {
-                var addFailedCountResponse = httpClient.PostAsJsonAsync("api/failedCounter/Add", account).Result; 
+                var addFailedCountResponse = httpClient.PostAsJsonAsync("api/failedCounter/Add", account).Result;
                 addFailedCountResponse.EnsureSuccessStatusCode();
+
+                var failedCountResponse =
+                    httpClient.PostAsJsonAsync("api/failedCounter/GetFailedCount", account).Result;
+
+                failedCountResponse.EnsureSuccessStatusCode();
+
+                var failedCount = failedCountResponse.Content.ReadAsAsync<int>().Result;
+                var logger = LogManager.GetCurrentClassLogger();
+                logger.Info($"accountId:{account} failed times:{failedCount}");
 
                 string message = $"account:{account} try to login failed";
                 var slackClient = new SlackClient("my api token");
