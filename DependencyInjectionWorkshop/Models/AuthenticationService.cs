e9ca9d8debcb622525d0deef27d049d1114f7a07
Joey Chen <jay.hatelove@gmail.com>
2023-02-04 11:54:40 +0800

    refactor: extract method for Composed Method to align sequence diagram

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -16,34 +16,73 @@ namespace DependencyInjectionWorkshop.Models
         { 
             //check account is locked
             var httpClient = new HttpClient() {BaseAddress = new Uri("http://joey.com/")};
-            var isLockedResponse = await httpClient.PostAsJsonAsync("api/failedCounter/IsLocked", account);
-
-            isLockedResponse.EnsureSuccessStatusCode();
-            if (await isLockedResponse.Content.ReadAsAsync<bool>())
+            
+            var isLocked = await IsLocked(account, httpClient);
+            if (isLocked)
             {
                 throw new FailedTooManyTimesException(){Account = account};
             }
             
-            //get password from DB
-            string passwordFromDb;
-            using (var connection = new SqlConnection("my connection string"))
+            var passwordFromDb = GetPasswordFromDb(account);
+
+            var hashedPassword = GetHashedPassword(password);
+
+            var currentOtp = await GetCurrentOtp(account, httpClient);
+
+            //check valid
+            if (passwordFromDb == hashedPassword && otp == currentOtp)
             {
-                passwordFromDb = connection.Query<string>("spGetUserPassword", new { Id = account },
-                                                          commandType: CommandType.StoredProcedure)
-                                           .SingleOrDefault();
+                await ResetFailedCount(account, httpClient);
+
+                return true;
             }
+            else
+            { 
+                await AddFailedCount(account, httpClient);
 
-            //hash input password
-            var crypt = new System.Security.Cryptography.SHA256Managed();
-            var hash = new StringBuilder();
-            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
-            foreach (var theByte in crypto)
-            {
-                hash.Append(theByte.ToString("x2"));
+                LogFailedCount(account, httpClient);
+
+                Notify($"account:{account} try to login failed");
+
+                return false;
             }
+        }
 
-            var hashResult = hash.ToString();
+        private static void Notify(string message)
+        {
+            //notify user
+            var slackClient = new SlackClient("my api token");
+            slackClient.PostMessage(response1 => { }, "my channel", message, "my bot name");
+        }
 
+        private static void LogFailedCount(string account, HttpClient httpClient)
+        {
+            //驗證失敗，紀錄該 account 的 failed 總次數 
+            var failedCountResponse =
+                httpClient.PostAsJsonAsync("api/failedCounter/GetFailedCount", account).Result;
+
+            failedCountResponse.EnsureSuccessStatusCode();
+
+            var failedCount = failedCountResponse.Content.ReadAsAsync<int>().Result;
+            var logger = NLog.LogManager.GetCurrentClassLogger();
+            logger.Info($"accountId:{account} failed times:{failedCount.ToString()}");
+        }
+
+        private static async Task AddFailedCount(string account, HttpClient httpClient)
+        {
+            //失敗
+            var addFailedCountResponse = await httpClient.PostAsJsonAsync("api/failedCounter/Add", account);
+            addFailedCountResponse.EnsureSuccessStatusCode();
+        }
+
+        private static async Task ResetFailedCount(string account, HttpClient httpClient)
+        {
+            var resetResponse = await httpClient.PostAsJsonAsync("api/failedCounter/Reset", account);
+            resetResponse.EnsureSuccessStatusCode();
+        }
+
+        private static async Task<string> GetCurrentOtp(string account, HttpClient httpClient)
+        {
             //get current otp
             var response = await httpClient.PostAsJsonAsync("api/otps", account);
             if (response.IsSuccessStatusCode)
@@ -55,38 +94,45 @@ namespace DependencyInjectionWorkshop.Models
             }
 
             var currentOtp = await response.Content.ReadAsAsync<string>();
+            return currentOtp;
+        }
 
-            //check valid
-            if (passwordFromDb == hashResult && otp == currentOtp)
+        private static string GetHashedPassword(string password)
+        {
+            //hash input password
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new StringBuilder();
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
+            foreach (var theByte in crypto)
             {
-                var resetResponse = await httpClient.PostAsJsonAsync("api/failedCounter/Reset", account);
-                resetResponse.EnsureSuccessStatusCode();
-                
-                return true;
+                hash.Append(theByte.ToString("x2"));
             }
-            else
-            { 
-                //失敗
-                var addFailedCountResponse = await httpClient.PostAsJsonAsync("api/failedCounter/Add", account);
-                addFailedCountResponse.EnsureSuccessStatusCode();
-                
-                //驗證失敗，紀錄該 account 的 failed 總次數 
-                var failedCountResponse =
-                    httpClient.PostAsJsonAsync("api/failedCounter/GetFailedCount", account).Result;
-    
-                failedCountResponse.EnsureSuccessStatusCode();
-    
-                var failedCount = failedCountResponse.Content.ReadAsAsync<int>().Result;
-                var logger = NLog.LogManager.GetCurrentClassLogger();
-                logger.Info($"accountId:{account} failed times:{failedCount.ToString()}");
-                
-                //notify user
-                string message = $"account:{account} try to login failed";
-                var slackClient = new SlackClient("my api token");
-                slackClient.PostMessage(response1 => { }, "my channel", message, "my bot name");
-                
-                return false;
+
+            var hashResult = hash.ToString();
+            return hashResult;
+        }
+
+        private static string GetPasswordFromDb(string account)
+        {
+            //get password from DB
+            string passwordFromDb;
+            using (var connection = new SqlConnection("my connection string"))
+            {
+                passwordFromDb = connection.Query<string>("spGetUserPassword", new { Id = account },
+                                                          commandType: CommandType.StoredProcedure)
+                                           .SingleOrDefault();
             }
+
+            return passwordFromDb;
+        }
+
+        private static async Task<bool> IsLocked(string account, HttpClient httpClient)
+        {
+            var isLockedResponse = await httpClient.PostAsJsonAsync("api/failedCounter/IsLocked", account);
+
+            isLockedResponse.EnsureSuccessStatusCode();
+            var isLocked = await isLockedResponse.Content.ReadAsAsync<bool>();
+            return isLocked;
         }
     }
 
