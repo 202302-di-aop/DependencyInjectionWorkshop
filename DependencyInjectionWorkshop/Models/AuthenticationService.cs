63c5a89184a0e66b608ee50e13a9da3b64b76e00
Joey Chen <jay.hatelove@gmail.com>
2023-02-05 15:28:48 +0800

    clean up

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -12,33 +12,6 @@ namespace DependencyInjectionWorkshop.Models
         Task<bool> Verify(string account, string password, string otp);
     }
 
-    public class FailCounterDecorator : IAuth
-    {
-        private readonly IAuth _auth;
-        private readonly IFailCounter _failCounter;
-
-        public FailCounterDecorator(IAuth auth, IFailCounter failCounter)
-        {
-            _auth = auth;
-            _failCounter = failCounter;
-        }
-
-        public async Task<bool> Verify(string account, string password, string otp)
-        {
-            await CheckAccountLocked(account);
-
-            return await _auth.Verify(account, password, otp);
-        }
-
-        private async Task CheckAccountLocked(string account)
-        {
-            if (await _failCounter.IsLocked(account))
-            {
-                throw new FailedTooManyTimesException() { Account = account };
-            }
-        }
-    }
-
     public class AuthenticationService : IAuth
     {
         private readonly IFailCounter _failCounter;
---
+++ DependencyInjectionWorkshop/Models/FailCounterDecorator.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+
+namespace DependencyInjectionWorkshop.Models
+{
+    public class FailCounterDecorator : IAuth
+    {
+        private readonly IAuth _auth;
+        private readonly IFailCounter _failCounter;
+
+        public FailCounterDecorator(IAuth auth, IFailCounter failCounter)
+        {
+            _auth = auth;
+            _failCounter = failCounter;
+        }
+
+        public async Task<bool> Verify(string account, string password, string otp)
+        {
+            await CheckAccountLocked(account);
+
+            return await _auth.Verify(account, password, otp);
+        }
+
+        private async Task CheckAccountLocked(string account)
+        {
+            if (await _failCounter.IsLocked(account))
+            {
+                throw new FailedTooManyTimesException() { Account = account };
+            }
+        }
+    }
+}
